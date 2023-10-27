5cf7d0d3e1b318c3a7241b81ae86be4c9fa49775
norhan204 <148470786+norhan204@users.noreply.github.com>
2023-10-27 23:23:56 +0200

    Update and rename caeser.py to caeser_bruteforce.py

--- caeser_bruteforce.py
+++ caeser_bruteforce.py
@@ -4,7 +4,7 @@ Auther: nour
 '''
 
 import sys 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'
 ciphertext = ''
 for arg in '\n'.join(sys.argv[1:28:8]): 
     ciphertext += arg 
